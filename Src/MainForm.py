0d0da114d0a0bc1f3cff476329c16a46e7c90e28
binbin6106 <zhangwenbin0707@gmail.com>
2020-03-31 11:41:32 +0800

    some changes

--- Src/MainForm.py
+++ Src/MainForm.py
@@ -4,8 +4,10 @@ from PySide2.QtGui import *
 from PySide2.QtCore import *
 import cv2
 from threading import Thread
-# from hyperlpr import pipline as pp
+from hyperlpr import pipline as pp
+import SqlTools
 import SerPort
+import PlateInput
 import time
 
 
@@ -13,25 +15,36 @@ class MainFormUI(QDialog):
     def __init__(self):
         # 从文件中加载UI定义
         super(MainFormUI, self).__init__()
+
+        # 定义一些属性
+        self.taken_photo_flag = 0
         # 从 UI 定义中动态 创建一个相应的窗口对象
         # 注意：里面的控件对象也成为窗口对象的属性了
         # 比如 self.ui.button , self.ui.textEdit
         self.ui = QUiLoader().load('../Gui/MainForm.ui')
-        # 载入界面
+        # 载入子页面
         self.ser_config = SerPort.SerPortTools()
+        self.plate_input = PlateInput.PlateInputTools()
+        self.sql_tools = SqlTools.Tools()
+        # 界面设置
         self.ui.statusbar.addWidget(self.ui.label_3)
-
+        self.ui.statusbar.addWidget(self.ui.label_2)
+        self.ui.label_2.setAutoFillBackground(True)  # 设置背景充满，为设置背景颜色的必要条件
+        self.set_label_2_color('red')
+        # 跳转相关界面
+        self.ui.action_5.triggered.connect(self.open_plate_input)  # 录入车牌
+        self.ui.action_8.triggered.connect(self.open_ser_config)  # 串口设置
+        # 测试用
         # self.ui.pushButton_5.clicked.connect(self.button_clicked)
-        self.ui.pushButton_5.clicked.connect(self.test)
-        self.ui.action_8.triggered.connect(self.open_ser_config)
+        self.ui.pushButton_5.clicked.connect(self.button_clicked)
 
         # 创建线程
         self.thead = Thread(target=self.update_serial_info, name=None)
-        self.thead.setDaemon(True) # 设置为守护线程
-        self.thead.start() #启动串口监听
+        self.thead.setDaemon(True)  # 设置为守护线程
+        self.thead.start()  # 启动串口监听
 
         # 创建定时器
-        self.timer = QTimer() #显示时间
+        self.timer = QTimer()  # 显示时间
         self.timer.timeout.connect(self.showTime)
         self.timer.start(1000)
 
@@ -43,6 +56,9 @@ class MainFormUI(QDialog):
     def open_ser_config(self):
         self.ser_config.ui.show()
 
+    def open_plate_input(self):
+        self.plate_input.ui.show()
+
     def openfile(self):
         openfile_name, _ = QFileDialog.getOpenFileName(self.ui, '选择文件', '', 'Jpeg files(*.jpg , *.jpeg)')
         return openfile_name
@@ -53,6 +69,28 @@ class MainFormUI(QDialog):
         # print(res[0])
         self.ui.label.setText(res[0])
         self.ui.label.adjustSize()
+        check_res = self.sql_tools.check_plate(res[0])
+        # print(check_res)
+        if check_res[0]:
+            if check_res[1][4] == '0':
+                now_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                self.ui.plainTextEdit.appendPlainText('车牌号:{}, 进入时间{}'.format(check_res[1][1], now_time))
+                self.sql_tools.update_intime_plate_info(now_time, check_res[1][0])
+                self.ui.plainTextEdit.appendPlainText('匹配成功，正在下发抬杆指令...')
+                time.sleep(0.01)
+                self.ui.plainTextEdit.appendPlainText('下发成功，正在抬杆')
+        else:
+            self.ui.plainTextEdit.appendPlainText('外来车辆，不予抬杆...')
+
+    def set_label_2_color(self, color):
+        pe = QPalette()
+        # pe.setColor(QPalette.WindowText, Qt.red)  # 设置字体颜色
+        if color == 'red':
+            pe.setColor(QPalette.Window, Qt.red)  # 设置背景颜色
+            self.ui.label_2.setPalette(pe)
+        else:
+            pe.setColor(QPalette.Window, Qt.green)  # 设置背景颜色
+            self.ui.label_2.setPalette(pe)
 
     def button_clicked(self):
         filename = self.openfile()
@@ -61,7 +99,16 @@ class MainFormUI(QDialog):
     def showTime(self):
         # 获取系统当前时间
         now_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        self.ui.label_3.setText(now_time)
+        self.ui.label_3.setText('当前时间:{}'.format(now_time))
+        if self.ser_config.is_open:
+            self.set_label_2_color('green')
+            self.ui.label_2.setText('设备状态:已连接')
+        else:
+            self.set_label_2_color('red')
+            self.ui.label_2.setText('设备状态:未连接')
+
+    def take_photo(self):
+        pass
 
     def update_serial_info(self):
         while True:
@@ -69,8 +116,11 @@ class MainFormUI(QDialog):
                 try:
                     serial_info = self.ser_config.recieve_mess()
                     if serial_info != '':
-                        # self.ui.plainTextEdit.appendPlainText(serial_info)
-                        print(serial_info)
+                        rec_mess = (str(serial_info, encoding='utf-8'))
+                        if rec_mess:  # 下位机发来拍照信号
+                            if self.taken_photo_flag == 0:
+                                self.take_photo()
+                                self.taken_photo_flag = 1
                 except Exception as e:
                     continue
             time.sleep(0.1)
@@ -90,7 +140,6 @@ class MainFormUI(QDialog):
             event.accept()
         else:
             event.ignore()
-
 
 
 if __name__ == '__main__':
--- Src/SqlTools.py
+++ Src/SqlTools.py
@@ -15,7 +15,13 @@ class Tools:
               "NAME TEXT," \
               "PASSWORD TEXT" \
               ")".format(sql_name)
-        sql_plate_info = ""
+        sql_plate_info = "CREATE TABLE IF NOT EXISTS {}(" \
+              "ID INTEGER PRIMARY KEY AUTOINCREMENT, " \
+              "plate TEXT," \
+              "intime TEXT," \
+              "outime TEXT," \
+              "isin TEXT" \
+              ")".format(sql_name)
         if table_type == 'user':
             final_sql = sql_user_info
         else:
@@ -60,3 +66,38 @@ class Tools:
         else:
             return [0, '用户不存在！']
 
+    def input_plate(self, info):
+        sql = "INSERT INTO plateinfo (plate, isin) VALUES ('{}', '{}')".format(info, '0')
+        try:
+            self.c.execute(sql)
+            self.conn.commit()
+            return [1, '录入成功!']
+        except Exception as e:
+            return [0, '录入失败!原因{}'.format(e)]
+
+    def check_plate(self, plate_num):
+        sql = "SELECT * from plateinfo where plate is '{}'".format(plate_num)
+        res = self.c.execute(sql)
+        data = res.fetchone()
+        if data:
+            return [1, data]
+        else:
+            return [0]
+
+    def update_intime_plate_info(self, time, plate_id):
+        sql = "UPDATE plateinfo SET intime = '{}', isin = '1' WHERE ID = '{}'".format(time, plate_id)
+        try:
+            self.c.execute(sql)
+            self.conn.commit()
+            return [1, '进入登记成功!']
+        except Exception as e:
+            return [0, '进入登记失败!原因{}'.format(e)]
+
+    def update_outime_plate_info(self, time, plate_id):
+        sql = "UPDATE plateinfo SET outime = '{}', isin = '0' WHERE ID = '{}'".format(time, plate_id)
+        try:
+            self.c.execute(sql)
+            self.conn.commit()
+            return [1, '驶出登记成功!']
+        except Exception as e:
+            return [0, '驶出登记失败!原因{}'.format(e)]
